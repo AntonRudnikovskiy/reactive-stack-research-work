435582ced574ebef3266f6b6abfb560c82660eed
Fyodor Losevskiy <losevskiyfz@gmail.com>
2024-08-15 22:43:07 +0300

    Initialized the project with spring.start.io

---
+++ src/main/java/com/losevskiyfz/reactivestackresearchwork/ReactiveStackResearchWorkApplication.java
@@ -0,0 +1,13 @@
+package com.losevskiyfz.reactivestackresearchwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStackResearchWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStackResearchWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/losevskiyfz/reactivestackresearchwork/ReactiveStackResearchWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losevskiyfz.reactivestackresearchwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStackResearchWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
